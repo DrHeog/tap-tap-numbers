5980b80fca2660e8e0dc7172c8f6e8b6f1ee6c46
FallenNephalem <ldr200@mail.ru>
2021-08-09 20:33:10 +0300

    the mechanism of changing rooms & change main scene

---
+++ scripts/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var lobby : Node2D = preload("res://scenes/лобби/DungeonRoom#1.tscn").instance()
+var dungeon : Node2D = preload("res://scenes/комната №2/DungeonRoom#2.tscn").instance()
+var timer : float = 0
+var first_go : bool = true
+
+func _ready():
+	self.add_child(lobby)
+	
+func _process(delta):
+	timer += delta
+	if timer > 10 and first_go:
+		lobby.queue_free()
+		self.add_child(dungeon)
+		first_go = false
